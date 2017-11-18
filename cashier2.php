25018e9f2e69aedeac22f1ba156b7d9b4c7e21bf
lovelyamandy <lovelyamandy18@gmail.com>
2017-11-18 09:55:48 +0800

    latest progress

---
+++ cashier2.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+	
+<head>
+	<title>POS</title>
+	<link href="css/style.css" type="text/css" rel="stylesheet" />
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+  
+</head>
+	<body>
+		
+		  <div class="asd col-xs-12">
+		  		<div class="container">
+		  		 	<div class="row">
+		  		 		<div class="col-lg-4">
+		  		 			<img src="images/logo.png" class="img-responsive">
+		  		 		</div>
+		  		 		<div class="col-lg-4">
+		  		 			<p class="order">Order no. 000</p>
+		  		 		</div>
+		  		 		<div class="col-lg-4">
+		  		 			<div class="form-group">
+                        <input type="text" name="search" placeholder="search" class="form-control" value="<?php $search?>"></div>
+		  		 			
+		  		 		</div>
+		  		 	</div>
+
+		  		 	<div class="row">
+		  		 		<div class="receipt col-lg-5">
+
+		  		 			<p class="StoreName">Aimm's Meat and More</p>
+		  		 			<br>
+		  		 			<p class="StoreName" ">-------------------------------------------------------------</p><br>
+		  		 			<tr style="padding: 50px">
+		  		 				<th>Name</th>
+		  		 				<th class="field">Qty</th>
+		  		 				<th class="field">Each</th>
+		  		 				<th class="field">Total</th>
+		  		 			</tr>
+		  		 			<p class="field"></p>
+		  		 		</div>
+		  		 	</div>
+		  		</div>
+
+		  </div>
+
+
+	</body>
+	</html>
+
